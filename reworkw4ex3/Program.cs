41b93ef89a40ba54510a046744bf50d9449d1f35
s-debug-alt <smeyer29@amarillocollege.com>
2025-04-28 12:35:07 -0500

    Add project files.

---
+++ reworkw4ex3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reworkw4ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // init a 2x3 matrix
+          int[,] matrix = new int[,] { 
+           { 1, 2, 3 }, { 4, 5, 6 } 
+            };
+            // loop through rows
+            for (int i =0; i <matrix.GetLength(0); i++)
+            { // Loop through columns
+
+              for (int j= 0; j < matrix.GetLength(1); j++) 
+                { // display matrix 
+                    Console.Write(matrix[i, j] + "\t"); 
+                } // enter newline for rows
+                Console.WriteLine();
+            }
+            // declare sum by tabular
+         int sum  = 0;
+            // go through each element
+            foreach (int i in matrix)
+            {
+
+
+                sum += i; // add sum to i
+            }
+            // display sum
+            Console.WriteLine("the sum is " + sum);
+
+            
+
+
+             // get the number of rows in the original matrix
+             int rows = matrix.GetLength(0);
+             // get the number of columns in the original matrix
+             int cols =  matrix.GetLength(1);
+             // Create a transposed matrix (2x3)
+             int[,]  transposed =new int[cols, rows];
+
+
+            // loop through matrix rows
+           for (int i = 0; i < rows; i++)
+            {   //loop through matrix columns
+              for (int j = 0; j < cols; j++)
+                  { // Swap row and column
+                   transposed[j, i] = matrix[i, j]; 
+                 }
+            }
+            // Loop through rows
+            for (int i=0; i < transposed.GetLength(0); i++)
+            {     // Loop through columns
+                for (int j =0; j < transposed.GetLength(1); j++) 
+                { // display transposed by tabular
+                  Console.Write(transposed[i, j] + "\t");
+                } // enter a newline for rows
+              Console.WriteLine(); } 
+            // wait for input before closing 
+            Console.Read();
+        }
+    }
+ }
+
---
+++ reworkw4ex3/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("reworkw4ex3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("reworkw4ex3")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6af65e87-f134-4977-a871-cdec485f425c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
